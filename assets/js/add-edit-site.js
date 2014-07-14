aa78eba9aa5e57062c87fe2d12e77d352d0dac45
cboss24 <cole_bosmann@hotmail.com>
2014-07-13 22:14:39 -0700

    added front end for sites / gestures

---
+++ assets/js/add-edit-site.js
@@ -0,0 +1,26 @@
+$(document).ready(function(){
+
+// PLACEHOLDER WILL COME LATER
+
+
+ /*
+    var url = "http://airauth.cloudnode.co/api/gesture/get";
+	var formData = {name:"ravi",age:"31"}; //Array 
+ 
+$.ajax({
+    url : "AJAX_POST_URL",
+    type: "POST",
+    data : formData,
+    success: function(data, textStatus, jqXHR)
+    {
+    },
+    error: function (jqXHR, textStatus, errorThrown)
+    {
+ 
+    }
+});
+          
+    return false;
+
+    */
+});
