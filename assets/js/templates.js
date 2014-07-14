110fd00d4d05c1b35904f5bbf830e66dd2bab51d
Conrad Foucher <conrad@foucher.ca>
2014-07-13 22:41:22 -0700

    missing files from last commit - virtual hand for launcher

--- assets/js/templates.js
+++ assets/js/templates.js
@@ -1,3 +1,5 @@
 // Include navbar
 var navbar_html = new EJS({url: '../../templates/navbar.ejs'});
 $('#navbar').html(navbar_html.text);
+var navbar__launcher_html = new EJS({url: '../../templates/navbar-launcher.ejs'});
+$('#navbar_launcher').html(navbar__launcher_html.text);
